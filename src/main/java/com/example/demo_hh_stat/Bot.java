f35a38fe97de07c200777a73aaa409ffda232963
reeffeer <Vfcnthgthytn1>
2023-06-07 19:22:55 +0300

    B классе HhApi использовал UriComponentsBuilder для построения URI запроса, а в методе getIdRegion(String nameRegion) вместо двойного вложенного цикла использовал обработку в потоках (stream). A поле salary в классе Filter сделал типа Integer.

--- src/main/java/com/example/demo_hh_stat/Bot.java
+++ src/main/java/com/example/demo_hh_stat/Bot.java
@@ -4,7 +4,12 @@ package com.example.demo_hh_stat;
 import com.example.demo_hh_stat.entity.Vacancy;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.File;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.KeyboardButton;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,64 +18,104 @@ import java.util.List;
 
 @Component
 public class Bot {
-    private Filter filter;
     private final Logic logic;
     private String botToken;
+    private enum BotState {
+        START, TITLE, CITY, EXPERIENCE, SALARY
+    }
+    private BotState currentState = BotState.START;
+    private Filter filter;
 
     public Bot(Logic logic, @Value("${bot.token}") String botToken) {
         this.logic = logic;
         this.botToken = botToken;
-        start(logic, botToken);
+        //start(logic, botToken);
+        start();
     }
 
-    private void start(Logic logic, String botToken) {
+    public void start() {
         TelegramBot bot = new TelegramBot(botToken);
-        bot.setUpdatesListener(element -> {
-            System.out.println(element);
-            element.forEach(it -> {
-                String massive[] = it.message().text().split(" ");
-                if (massive[0].equals("/start") || massive.length < 2){
-                    bot.execute(new SendMessage(it.message().chat().id(), "Привет! Я покажу тебе количество откликов на интересующую тебя позицию " +
-                            "в интересующем тебя городе, а также средний конкурс." +
-                            "\nВведи название вакансии(одним словом) и город поиска в виде:" + "\nВакансия Город" + "\nЕсли название города поиска неуникально, введи в формате:" +
-                            "\nВакансия Город (Область)"));
-                } else {
-                    if (massive.length > 2){
-                        massive[1] = overRide(massive);
-                    }
-                    String requestId = String.valueOf(it.message().messageId()).concat(String.valueOf(it.message().from().id()));
-                    filter = new Filter.FilterBuilder()
-                            .title(massive[0])
-                            .region(massive[1])
-                            .requestId(requestId)
-                            .build();
-
-                    List<Vacancy> vacancies = logic.getVacancyFilter(filter);
-                    int numVacancies = vacancies.size();
-                    int allResponses = logic.getAllResponses(vacancies);
-
-                    if (vacancies.size() != 0){
-
-                        //show all vacancies
-                        /*list.forEach(vacancy -> {
-                            bot.execute(new SendMessage(it.message().chat().id(), "Вакансия: " + vacancy.getName() + "\nКоличество откликов: " + vacancy.getCounters().getResponses() + "\nСсылка: http://hh.ru/vacancy/" + vacancy.getId()));
-                            System.out.println(vacancy.getId() + " " + vacancy.getName());
-                        });*/
-                        bot.execute(new SendMessage(it.message().chat().id(), "Количество найденных вакансий: " + vacancies.size() + " и всего откликов " + allResponses + "."));
-                    } else {
-                        bot.execute(new SendMessage(it.message().chat().id(), "По вашему запросу вакансий не найдено."));
-                    }
-                }
-            });
+        bot.setUpdatesListener(updates -> {
+            updates.forEach(this::processUpdate);
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
     }
 
-    public String overRide(String s[]){
-        String reg = s[1];
-        for (int i = 2; i < s.length; i++){
-            reg = reg + " " + s[i];
+    private void processUpdate(Update update) {
+        if (update.message() != null) {
+            long chatId = update.message().chat().id();
+            String messageText = update.message().text();
+
+            switch (currentState) {
+                case START:
+                    handleStartCommand(chatId);
+                    break;
+                case TITLE:
+                    handleTitleInput(chatId, messageText);
+                    break;
+                case CITY:
+                    handleCityInput(chatId, messageText);
+                    break;
+                case EXPERIENCE:
+                    handleExperienceInput(chatId, messageText);
+                    break;
+                case SALARY:
+                    handleSalaryInput(chatId, messageText);
+                    break;
+            }
         }
-        return reg;
+    }
+
+    private void handleStartCommand(long chatId) {
+        sendMessage(chatId, "Привет! Я покажу вам количество откликов на интересующую вас позицию " +
+                "в интересующем вас городе, а также средний конкурс.\n" +
+                "Введите название вакансии: ");
+        currentState = BotState.TITLE;
+        filter = new Filter();
+    }
+
+    private void handleTitleInput(long chatId, String title) {
+        filter.setTitle(title);
+        sendMessage(chatId, "Введите название города:");
+        currentState = BotState.CITY;
+    }
+
+    private void handleCityInput(long chatId, String city) {
+        filter.setRegion(city);
+        sendMessage(chatId, "Введите опыт работы (если не указывать, введите 0):");
+        currentState = BotState.EXPERIENCE;
+    }
+
+    private void handleExperienceInput(long chatId, String experience) {
+        if (!experience.equals("0")) {
+            filter.setExperience(experience);
+        }
+        sendMessage(chatId, "Введите желаемую зарплату (если не указывать, введите 0):");
+        currentState = BotState.SALARY;
+    }
+
+    private void handleSalaryInput(long chatId, String salary) {
+        if (!salary.equals("0")) {
+            filter.setSalary(Integer.parseInt(salary));
+        }
+
+        List<Vacancy> vacancies = logic.getVacancyFilter(filter);
+        int numVacancies = vacancies.size();
+        int allResponses = logic.getAllResponses(vacancies);
+
+        if (numVacancies != 0) {
+            sendMessage(chatId, "Найдено вакансий: " + numVacancies + " и всего откликов: " + allResponses);
+        } else {
+            sendMessage(chatId, "По вашему запросу вакансий не найдено.");
+        }
+
+        currentState = BotState.START;
+        filter = null;
+    }
+
+    private void sendMessage(long chatId, String message) {
+        TelegramBot bot = new TelegramBot(botToken);
+        SendResponse response = bot.execute(new SendMessage(chatId, message));
+        System.out.println(response.isOk());
     }
 }
--- src/main/java/com/example/demo_hh_stat/Logic.java
+++ src/main/java/com/example/demo_hh_stat/Logic.java
@@ -10,22 +10,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class Logic {
     private final HhApi hhApi;
-
     public List<Vacancy> getVacancyFilter(Filter filter){
         Field field[] = filter.getClass().getDeclaredFields();
         field[2].setAccessible(true);
         field[3].setAccessible(true);
         try {
-            if (field[2].get(filter) == null && Integer.parseInt(String.valueOf(field[3].get(filter))) == 0){
+            if (field[2].get(filter) == null && field[3].get(filter) == null) {
                 return hhApi.getVacanciesFilterNameRegion(filter.getTitle(),filter.getRegion());
             }
-            if (field[2].get(filter) != null && Integer.parseInt(String.valueOf(field[3].get(filter))) == 0){
+            if (field[2].get(filter) != null && field[3].get(filter) == null) {
                 return hhApi.getVacanciesFilterNameRegionExperience(filter.getTitle(),filter.getRegion(),filter.getExperience());
             }
-            if (field[2].get(filter) == null && Integer.parseInt(String.valueOf(field[3].get(filter))) != 0){
+            if (field[2].get(filter) == null && field[3].get(filter) != null){
                 return hhApi.getVacanciesFilterNameRegionSalary(filter.getTitle(),filter.getRegion(),String.valueOf(filter.getSalary()));
             }
-            if (field[2].get(filter) != null && Integer.parseInt(String.valueOf(field[3].get(filter))) != 0){
+            if (field[2].get(filter) != null && field[3].get(filter) != null){
                 return hhApi.getVacanciesFilterNameRegionExperienceSalary(filter.getTitle(), filter.getRegion(), filter.getExperience(), String.valueOf(filter.getSalary()));
             }
             return null;
