e15151174e4cdcdce4bf748b808d5ced74e95213
Kirill Korsuk <korsukkirill@gmail.com>
2023-06-10 12:02:47 +0300

    remove reflection

--- src/main/java/com/example/demo_hh_stat/Logic.java
+++ src/main/java/com/example/demo_hh_stat/Logic.java
@@ -4,33 +4,24 @@ import com.example.demo_hh_stat.entity.Vacancy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class Logic {
     private final HhApi hhApi;
+
     public List<Vacancy> getVacancyFilter(Filter filter){
-        Field field[] = filter.getClass().getDeclaredFields();
-        field[2].setAccessible(true);
-        field[3].setAccessible(true);
-        try {
-            if (field[2].get(filter) == null && field[3].get(filter) == null) {
-                return hhApi.getVacanciesFilterNameRegion(filter.getTitle(),filter.getRegion());
-            }
-            if (field[2].get(filter) != null && field[3].get(filter) == null) {
-                return hhApi.getVacanciesFilterNameRegionExperience(filter.getTitle(),filter.getRegion(),overRideExperience(filter.getExperience()));
-            }
-            if (field[2].get(filter) == null && field[3].get(filter) != null){
-                return hhApi.getVacanciesFilterNameRegionSalary(filter.getTitle(),filter.getRegion(),String.valueOf(filter.getSalary()));
-            }
-            if (field[2].get(filter) != null && field[3].get(filter) != null){
-                return hhApi.getVacanciesFilterNameRegionExperienceSalary(filter.getTitle(), filter.getRegion(), overRideExperience(filter.getExperience()), String.valueOf(filter.getSalary()));
-            }
-            return null;
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
+        if(filter.getExperience() == null && filter.getSalary() == null)
+            return hhApi.getVacanciesFilterNameRegion(filter.getTitle(),filter.getRegion());
+        if(filter.getExperience() != null && filter.getSalary() == null)
+            return hhApi.getVacanciesFilterNameRegionExperience(filter.getTitle(),filter.getRegion(),overRideExperience(filter.getExperience()));
+        if(filter.getExperience() == null && filter.getSalary() != null)
+            return hhApi.getVacanciesFilterNameRegionSalary(filter.getTitle(),filter.getRegion(),String.valueOf(filter.getSalary()));
+        if(filter.getExperience() != null && filter.getSalary() != null)
+            return hhApi.getVacanciesFilterNameRegionExperienceSalary(filter.getTitle(), filter.getRegion(), overRideExperience(filter.getExperience()), String.valueOf(filter.getSalary()));
+        return new ArrayList<>();
     }
 
     public int getNumberOfVacancies(List<Vacancy> list) {
